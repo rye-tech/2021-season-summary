b2f33906527011155ba91d5ecf378c2ed045bdcf
Ryan Hartnett <rynocean@gmail.com>
2021-06-01 23:46:45 -0700

    Updated the name of the Bay Standards to match each other so the data from May 5th 2021 can process correctly. I also added in the next steps to process data from May 5th.

--- check sample work/christians samples 2021/TA/Alkalinity_Data_Pull_20210601.R
+++ check sample work/christians samples 2021/TA/Alkalinity_Data_Pull_20210601.R
@@ -16,7 +16,7 @@ getwd()
 setwd(here("check sample work", 
            "christians samples 2021",
            "TA",
-           "2021-04-28-run"))
+           "2021-05-05-run"))
 
 getwd()
 
@@ -43,14 +43,27 @@ list.files()
 
 # 4/28/2021
 
+# #   sample information file
+# file_1 <- "alk run 28apr2021 samples only.csv"  
+# 
+# #   sample environmental data
+# file_2 <- "20210428 sample metadata.csv"
+# 
+# # alkalinity titration data file
+# file_3 <-  "alk run 28apr2021 data only.csv"
+
+
+
+# 5/05/2021
+
 #   sample information file
-file_1 <- "alk run 28apr2021 samples only.csv"  
+file_1 <- "alk run 05may2021 samples only.csv"  
 
 #   sample environmental data
-file_2 <- "20210428 sample metadata.csv"
+file_2 <- "20210505 sample metadata.csv"
 
 # alkalinity titration data file
-file_3 <-  "alk run 28apr2021 data only.csv"
+file_3 <-  "alk run 05may2021 data only.csv"
 
 
 
