1971be452c7bac57d52f498e1d8c3d8d22bfe156
Ryan Hartnett <rynocean@gmail.com>
2021-07-03 17:06:48 -0700

    cleaning up the pH data processing script to not have "data" be the place holder for the data frame. also removed the "rm(list = ls())" since that's bad etiquette.
    updated the script to process the data from June 9th 2021, Christian's last day of processing.

--- check sample work/christians samples 2021/pH/pH processing script 20210601.R
+++ check sample work/christians samples 2021/pH/pH processing script 20210601.R
@@ -31,37 +31,33 @@ list.files()
 # [6] "20210505_results.csv"           
 # [8] "20210512_results.csv"           
 # [10] "20210519_results.csv"              
-
+# [17] "20210609_results.csv"
                  
-
-#clear all plots and data
-
-rm(list = ls())
 
 #enter your csv file title below
 
-filename <- "20210519_results"
+filename <- "20210609_results"
 
 results <- paste0(filename, "_processed")
 
 print(results)
 
 #read data table
-data <- read.csv(paste0(filename,".csv"))
+df <- read.csv(paste0(filename,".csv"))
 
 #add desired columns for calculations to work
 
-data$a1a2 <- NA
+df$a1a2 <- NA
 
-data$a1a2crct <- NA
+df$a1a2crct <- NA
 
-data$pH_is <- NA
+df$pH_is <- NA
 
-data$pH_a <- NA
+df$pH_a <- NA
 
-data$pH_is_crct <- NA
+df$pH_is_crct <- NA
 
-data$pH_a_crct <- NA
+df$pH_a_crct <- NA
 
 
 # The following variables and calculations are worked up as written in section 8.4 of
@@ -87,11 +83,11 @@ e3 <- 0.1331
 ####calculations ####
 
 
-# calculates length of data table
+# calculates length of df table
 # then creates sequence by 2 to pick the second row of each sample analyzed
 # so the for loop below can find where to pick up the data
 
-len <- seq(2, length(data$X578.nm) - 1, by = 2)
+len <- seq(2, length(df$X578.nm) - 1, by = 2)
 
 
 
@@ -100,7 +96,7 @@ for (i in len){
   
 #enter dye volume used in microliters (yes I know the variable is labled mL and not uL, b/c im lame and tired) #
   
-dyeV_mL <- data$dyeV[i+1]
+dyeV_mL <- df$dyeV[i+1]
     
 #volume converted to cubic cm
     
@@ -108,7 +104,7 @@ dyeV_cm3  <- dyeV_mL/1000
   
 # in situ temperature
   
-tsc <- data$temperature[i]
+tsc <- df$temperature[i]
   
 tsk <- tsc + 273.15  
   
@@ -120,7 +116,7 @@ tak <- tac + 273.15
   
 # in situ salinity    
   
-ss <- data$salinity[i]
+ss <- df$salinity[i]
   
 
   
@@ -137,8 +133,8 @@ pk2_a <- (1245.69/tak) +3.8275+0.00211*(35 - ss)
 # a1/a2 difference in absorbances
 
 
-a1a2 <- (data$X578.nm[i+1] - data$X578.nm[i] -(data$X730.nm[i+1] - data$X730.nm[i])) / 
-            (data$X434.nm[i+1] - data$X434.nm[i]-(data$X730.nm[i+1] - data$X730.nm[i]))
+a1a2 <- (df$X578.nm[i+1] - df$X578.nm[i] -(df$X730.nm[i+1] - df$X730.nm[i])) / 
+            (df$X434.nm[i+1] - df$X434.nm[i]-(df$X730.nm[i+1] - df$X730.nm[i]))
 
 a1a2crctd <- a1a2 - (dyeV_cm3 * (0.125 - 0.147*a1a2))
 
@@ -154,22 +150,22 @@ pH_analysis_crctd <- pk2_a + log10((a1a2crctd-e1)/(e2- (a1a2crctd * e3)))
 
 #append calculations to data frame
 
-data$a1a2[i] <- a1a2
+df$a1a2[i] <- a1a2
 
-data$a1a2crct[i] <- a1a2crctd
+df$a1a2crct[i] <- a1a2crctd
 
-data$pH_is[i] <- pH_in_situ
+df$pH_is[i] <- pH_in_situ
 
-data$pH_a[i] <- pH_analysis
+df$pH_a[i] <- pH_analysis
 
-data$pH_is_crct[i] <- pH_in_situ_crctd
+df$pH_is_crct[i] <- pH_in_situ_crctd
 
-data$pH_a_crct[i] <- pH_analysis_crctd
+df$pH_a_crct[i] <- pH_analysis_crctd
 
 
 }
 
 #saves a csv file appended with results
 
-write.csv(data, file = paste0(results,".csv"), row.names = F) 
+write.csv(df, file = paste0(results,".csv"), row.names = F) 
 
